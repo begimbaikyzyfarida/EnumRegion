eafb87795ee41f3d01c9033174866c27c11f37ff
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-18 19:10:59 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        Kyrgyzstan region1 = Kyrgyzstan.OSH;
+        System.out.println(region1 + " region " + region1.getDescription());
+        Kyrgyzstan region2 = Kyrgyzstan.BATKEN;
+        System.out.println("--------------------------------");
+        System.out.println(region2 + " region " + region2.getDescription());
+        Kyrgyzstan region3 = Kyrgyzstan.JALALABAD;
+        System.out.println("--------------------------------");
+        System.out.println(region3 + " region " + region3.getDescription());
+        Kyrgyzstan region4 = Kyrgyzstan.NARYN;
+        System.out.println("--------------------------------");
+        System.out.println(region4+ " region " + region4.getDescription());
+        Kyrgyzstan region5 = Kyrgyzstan.ISSYKKOL;
+        System.out.println("--------------------------------");
+        System.out.println(region5 + " region " + region5.getDescription());
+        Kyrgyzstan region6 = Kyrgyzstan.TALAS;
+        System.out.println("--------------------------------");
+        System.out.println(region6 + " region " + region6.getDescription());
+        Kyrgyzstan region7 = Kyrgyzstan.CHUI;
+        System.out.println("--------------------------------");
+        System.out.println(region7 + " region " + region7.getDescription());
+        System.out.println("--------------------------------");
+    }
+}
